ef30bb31045550bfd86f7f1375d7696df2f83eea
Festanny <60522731+Festanny@users.noreply.github.com>
2022-12-02 21:44:52 +0400

    upd

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,21 @@
-$('header .header nav .menuText .topmenuMain').hover(
-    function() {
-        $( this ).find(' > .submenu').fadeIn()
-    }, function() {
-        $( this ).find(' > .submenu').fadeOut()
-    }
-)
-$(window).resize(function() {
+(function($) {
+    $(window).resize(function() {
+        size()
+    });
     size()
-});
-size()
-function size() {
-    $('header .header nav .menuText .topmenuBig > .submenu .submenuP').width($('.container').width())
-    $('header .header nav .menuText .topmenuBig > .submenu .submenuP .tile').width(($('body').width() - $('.container').width()) / 2)
-}
+    function size() {
+        $('header .header nav .menuText .topmenuBig > .submenu .submenuP').width($('.container').width())
+        $('header .header nav .submenuL3 .submenu3 .submenu3-1 .line').width(($('body').width() - $('.container').width()) / 2)
+        $('header .header nav .submenuL3 .submenu3').css({
+            'top': $('header .header nav .menuText .topmenuBig > .submenu .submenu2').height()
+        })
+    }
+    setTimeout(function() {
+        $('header .header nav .submenuL3 .submenu3').css({
+            'top': $('header .header nav .menuText .topmenuBig > .submenu .submenu2').height()
+        })
+    }, 10)
+    $('.offcanvas#mobMenu .topmenu li .nextOpen > a').on('click', function() {
+        console.log($(this).parent('.nextOpen').next('.submenu').toggleClass('open'))
+    })
+})(jQuery);
